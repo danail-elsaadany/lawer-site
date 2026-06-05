5eb5e07348dac9aa6dc7fdbecb3d96c185e7eac4
Danail <sterlinglawservices@gmail.com>
2026-06-05 12:47:14 +0400

    TG

--- js/script.js
+++ js/script.js
@@ -187,7 +187,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 });
             }
         }, { threshold: 0.3 });
-س
+
         statsObserver.observe(statsSection);
     }
 
